79414d6614953020cceecc59d67094c120dcd8d8
nayannayak007 <142709977+nayannayak007@users.noreply.github.com>
2024-06-18 17:45:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
-
+from flask import Flask, jsonify
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/ping")
+def ping():
+    return jsonify({'message': 'pong'})
+@app.route("/")
+def index():
+    return jsonify({'message': 'hello world'})
+if __name__ == "__main__":
+    app.run(port=8000, host='0.0.0.0')
