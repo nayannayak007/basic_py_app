8ecc1b06a58b3f3caa870f3b29d6d69cb0c14817
nayannayak007 <142709977+nayannayak007@users.noreply.github.com>
2024-06-18 19:05:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from tabpy.tabpy_server.app.app import TabPyApp
 import threading
-# Define a function to start TabPy server
+import os
+
+# Define a function to start the TabPy server
 def start_tabpy_server():
-    # Start TabPy server
-    app = TabPyApp('tabpy.ini')
+    # Fetch the port from Azure environment or use a default
+    port = os.environ.get('WEBSITE_PORT', '9004')
+    # Configure the TabPy server with the port
+    app = TabPyApp(config_file='tabpy.ini', port=int(port))
+    # Start the server
     app.run()
+
 if __name__ == '__main__':
     # Start TabPy server in a separate thread
-    # tabpy_thread = threading.Thread(target=start_tabpy_server)
-    # tabpy_thread.start()
-    start_tabpy_server()
-    print("TabPy server started on port 9004.")
+    tabpy_thread = threading.Thread(target=start_tabpy_server)
+    tabpy_thread.start()
+    tabpy_thread.join()  # This ensures the main thread waits for the server thread
+    print(f"TabPy server started on port {port}.")
     print("You can now connect to TabPy and use it for predictive analytics.")
